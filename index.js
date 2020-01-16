b9837616ed129e11032d9ab58fee2a99b9408070
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-01-15 19:59:15 -0800

    Completed indetification by id

--- index.js
+++ index.js
@@ -190,8 +190,9 @@ function getLastCarInfo(inventory) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoById(/* code here */) {
-  /* code here */
+function getCarInfoById(inventory, id) {
+  let carid = inventory[0];
+  return 'This is a ' + carid.car_make + carid.car_model;
 }
 
 /**
@@ -202,8 +203,8 @@ function getCarInfoById(/* code here */) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(inventory) {
+  return inventory.sort;
 }
 
 /**
